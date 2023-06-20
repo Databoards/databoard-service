d7501a267079e9e937c90dd6d865508c05c06c9d
dunajilang <duna.j@licenseware.io>
2023-06-21 00:02:55 +0100

    added clocker-collections

--- utils/mongo_collections.py
+++ utils/mongo_collections.py
@@ -5,3 +5,13 @@ from dataclasses import dataclass
 class DATABOARD_COLLECTIONS:
     USERS: str = "organization"
     TAGS: str = "tags"
+
+
+@dataclass
+class CLOCKER_COLLECTIONS:
+    USERS: str = "users"
+    PROFESSIONAL_CARD: str = "professionalCard"
+    STUDENT_CARD: str = "studentCard"
+    PERSONAL_CARD: str = "personalCard"
+    CONTACTS: str = "contacts"
+    NOTIFICATIONS: str = "notifications"
