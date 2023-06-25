8d2f164d1243131b866c3c3714432a527d10d1b3
dunajilang <duna.j@licenseware.io>
2023-06-25 21:06:27 +0100

    wp

--- clock/clock_router.py
+++ clock/clock_router.py
@@ -11,13 +11,10 @@ from utils.mongo_connect import db
 
 router = APIRouter(tags=["Clock Routes"], prefix="/clocks")
 
-
 @router.get("/fetch_tag_clocks/{tag_id}")
-async def fetch_tag_clocks(
-    tag_id:str,current_user: User = Depends(service.get_current_user)
-):
+async def fetch_tag_clocks(tag_id: str, current_user: User = Depends(service.get_current_user)):
     try:
-        clocks=db[DATABOARD_COLLECTIONS.TAGS].find_one({"org_id":current_user.get("_id"),"_id": {tag_id}},projection={"clocks":True})
+        clocks = db[DATABOARD_COLLECTIONS.TAGS].find_one({"org_id": current_user.get("_id"), "_id": tag_id}, projection={"clocks": True})
         if clocks:
             user_ids = [clock.get("user_id") for clock in clocks]
             users = await db[CLOCKER_COLLECTIONS.USERS].find({"_id": {"$in": user_ids}}).to_list(length=None)
@@ -30,32 +27,32 @@ async def fetch_tag_clocks(
                 user_info = user_map.get(user_id)
                 if user_info:
                     clock.update({
-                        "tag_id": {tag_id},
+                        "tag_id": tag_id,
                         "email": user_info.get("email"),
                         "gender": user_info.get("gender"),
                         "age": user_info.get("age")
                     })
-        
+
                 clocks_with_users.append(clock)
             return {
-                    "status_code": status.HTTP_200_OK,
-                    "status": "success",
-                    "message": "Clocks successfully retrieved.",
-                    "data": clocks_with_users,
-                }
+                "status_code": status.HTTP_200_OK,
+                "status": "success",
+                "message": "Clocks successfully retrieved.",
+                "data": clocks_with_users,
+            }
         else:
-                return {
-                    "status_code": status.HTTP_200_OK,
-                    "status": "success",
-                    "message": "Clocks successfully retrieved but empty.",
-                    "data": [],
-                }
+            return {
+                "status_code": status.HTTP_200_OK,
+                "status": "success",
+                "message": "Clocks successfully retrieved but empty.",
+                "data": [],
+            }
     except Exception as e:
         return HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail={
                 "status": "error",
-                "message": "Opps! Something went wrong",
+                "message": "Oops! Something went wrong",
                 "data": f"This is the bug bro {e}",
             },
         )
