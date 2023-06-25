d84a31bfe778e4740b2ef58d1e2787f2a84bc4de
dunajilang <duna.j@licenseware.io>
2023-06-25 23:24:37 +0100

    wp

--- clock/clock_router.py
+++ clock/clock_router.py
@@ -15,21 +15,19 @@ router = APIRouter(tags=["Clock Routes"], prefix="/clocks")
 async def fetch_tag_clocks(tag_id: str, current_user: User = Depends(service.get_current_user)):
     try:
         tag_id_str = str(tag_id)  # Convert tag_id to a string
-        print(f"This is the tag_id bro {tag_id_str}")
 
         clocks = await db[DATABOARD_COLLECTIONS.TAGS].find_one({"org_id": current_user.get("_id"), "tag_code": tag_id_str}, projection={"clocks": True})
         clocks=clocks.get('clocks')
-        print(f"These are the clocks bro: {clocks}")
         if clocks:
             user_ids = [clock.get("user_id") for clock in clocks]
             users = await db[CLOCKER_COLLECTIONS.USERS].find({"_id": {"$in": user_ids}}).to_list(length=None)
+            print(f"These are the number of users: {users}")
 
             user_map = {str(user["_id"]): user for user in users}
-            print("This is a pass bro: 2")
 
             clocks_with_users = []
             for clock in clocks:
-                user_id = clock.get("user")
+                user_id = clock.get("user_id")
                 user_info = user_map.get(user_id)
                 if user_info:
                     clock.update({
@@ -40,13 +38,13 @@ async def fetch_tag_clocks(tag_id: str, current_user: User = Depends(service.get
                     })
 
                 clocks_with_users.append(clock)
+            print(f"These are the clocks bro: {clocks}")
             return {
                 "status_code": status.HTTP_200_OK,
                 "status": "success",
                 "message": "Clocks successfully retrieved.",
                 "data": clocks_with_users,
             }
-            print("My brother you need to see this")
         else:
             return {
                 "status_code": status.HTTP_200_OK,
